b5abdcee74b3914b8e10292e3fb113f64630ebe6
Tiffany Nogueira <tiffnogueira@gmail.com>
2015-11-09 16:26:40 -0500

    Updated style

--- functions.php
+++ functions.php
@@ -279,6 +279,12 @@ function addLinkClass($atts) {
 }
 
 
+add_filter( 'post_thumbnail_html', 'remove_thumbnail_dimensions', 10, 3 );
+
+function remove_thumbnail_dimensions( $html, $post_id, $post_image_id ) {
+    $html = preg_replace( '/(width|height)=\"\d*\"\s/', "", $html );
+    return $html;
+}
 
 
 
